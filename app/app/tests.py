510c1221dd2a842d94bbca722670d68add39eb70
Joshua Quesada <quesadaleonjoshua@gmail.com>
2024-11-08 20:43:57 -0600

    Add testing files

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTestCase(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test addition of two numbers."""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtraction of two numbers."""
+        res = calc.subtract(5, 6)
+        self.assertEqual(res, -1)
