80c90a09ce22679b3ff2072884fc95b5510b81e8
Joshua Quesada <quesadaleonjoshua@gmail.com>
2024-11-08 21:29:22 -0600

    Add wait_for_db command and fix linter errors:

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,14 +1,13 @@
 """
 Sample tests
 """
-
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTestCase(SimpleTestCase):
     """Test the calc module."""
-
     def test_add_numbers(self):
         """Test addition of two numbers."""
         res = calc.add(5, 6)
